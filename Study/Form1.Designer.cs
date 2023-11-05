8bcdaddeb46274afb4cc7d2373afa7029bf92ae0
asmodeliy@student.jbnu.ac.kr <asmodeliy@student.jbnu.ac.kr>
2023-11-05 19:35:45 +0900

    2nd

--- Study/Form1.Designer.cs
+++ Study/Form1.Designer.cs
@@ -28,21 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.metroCheckBox1 = new MetroFramework.Controls.MetroCheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.metroUserControl1 = new MetroFramework.Controls.MetroUserControl();
+            this.dataSet1 = new System.Data.DataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Study.Properties.Resources.ramschip1;
+            this.pictureBox1.Location = new System.Drawing.Point(570, 52);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(215, 57);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.YellowGreen;
+            this.button1.Location = new System.Drawing.Point(26, 188);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 84);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "doc";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.doc_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.YellowGreen;
+            this.button2.Location = new System.Drawing.Point(275, 188);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(238, 84);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "nc";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.nc_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.YellowGreen;
+            this.button3.Location = new System.Drawing.Point(524, 188);
+            this.button3.Margin = new System.Windows.Forms.Padding(6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(238, 84);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "vcs";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.vcs_Click);
+            // 
+            // metroCheckBox1
+            // 
+            this.metroCheckBox1.AutoSize = true;
+            this.metroCheckBox1.Location = new System.Drawing.Point(52, 638);
+            this.metroCheckBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.metroCheckBox1.Name = "metroCheckBox1";
+            this.metroCheckBox1.Size = new System.Drawing.Size(114, 15);
+            this.metroCheckBox1.TabIndex = 5;
+            this.metroCheckBox1.Text = "metroCheckBox1";
+            this.metroCheckBox1.UseSelectable = true;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.YellowGreen;
+            this.button4.Location = new System.Drawing.Point(524, 284);
+            this.button4.Margin = new System.Windows.Forms.Padding(6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(238, 84);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "gnet";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.gnet_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.YellowGreen;
+            this.button5.Location = new System.Drawing.Point(275, 284);
+            this.button5.Margin = new System.Windows.Forms.Padding(6);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(238, 84);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "lib";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.lib_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.YellowGreen;
+            this.button6.Location = new System.Drawing.Point(26, 284);
+            this.button6.Margin = new System.Windows.Forms.Padding(6);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(238, 84);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "lef";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.lef_Click);
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Location = new System.Drawing.Point(20, 978);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1940, 22);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
+            this.statusStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip1_ItemClicked);
+            // 
+            // metroUserControl1
+            // 
+            this.metroUserControl1.Location = new System.Drawing.Point(1165, 198);
+            this.metroUserControl1.Name = "metroUserControl1";
+            this.metroUserControl1.Size = new System.Drawing.Size(646, 150);
+            this.metroUserControl1.TabIndex = 11;
+            this.metroUserControl1.UseSelectable = true;
+            this.metroUserControl1.Load += new System.EventHandler(this.metroUserControl1_Load);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1980, 1020);
+            this.Controls.Add(this.metroUserControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.metroCheckBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(20, 120, 20, 20);
+            this.Style = MetroFramework.MetroColorStyle.Silver;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private MetroFramework.Controls.MetroCheckBox metroCheckBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private MetroFramework.Controls.MetroUserControl metroUserControl1;
+        private System.Data.DataSet dataSet1;
     }
 }
 
--- Study/Form1.cs
+++ Study/Form1.cs
@@ -12,6 +12,8 @@ namespace Study
 {
     public partial class Form1 : MetroFramework.Forms.MetroForm //상속 클래스 변경
     {
+        private object label1;
+
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +22,109 @@ namespace Study
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void metroTile1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void doc_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void nc_Click(object sender, EventArgs e)
+        {
+            ShowFileOpenDialog();
+        }
+
+        private void vcs_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lef_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lib_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gnet_Click(object sender, EventArgs e)
+        {
+
+        }
+        public string ShowFileOpenDialog()
+        {
+            //파일오픈창 생성 및 설정
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "파일 오픈 예제창";
+            ofd.FileName = "test";
+            ofd.Filter = "그림 파일 (*.jpg, *.gif, *.bmp) | *.jpg; *.gif; *.bmp; | 모든 파일 (*.*) | *.*";
+
+            //파일 오픈창 로드
+            DialogResult dr = ofd.ShowDialog();
+
+            //OK버튼 클릭시
+            if (dr == DialogResult.OK)
+            {
+                //File명과 확장자를 가지고 온다.
+                string fileName = ofd.SafeFileName;
+                //File경로와 File명을 모두 가지고 온다.
+                string fileFullName = ofd.FileName;
+                //File경로만 가지고 온다.
+                string filePath = fileFullName.Replace(fileName, "");
+
+                //출력 예제용 로직
+                label1 = "File Name  : " + fileName;
+               // label2.Text = "Full Name  : " + fileFullName;
+                //label3.Text = "File Path  : " + filePath;
+                //File경로 + 파일명 리턴
+                return fileFullName;
+            }
+            //취소버튼 클릭시 또는 ESC키로 파일창을 종료 했을경우
+            else if (dr == DialogResult.Cancel)
+            {
+                return "";
+            }
+
+            return "";
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void metroUserControl1_Load(object sender, EventArgs e)
+        {
+            if (label1 != null)
+            {
+                Console.Write(label1);
+            }
+            if (label1 != null)
+            {
+                Console.Write(label1);
+            }
         }
     }
 }
