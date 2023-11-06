6fa181fa4f696795c578c9b328926e393199ba2f
asmodeliy@student.jbnu.ac.kr <asmodeliy@student.jbnu.ac.kr>
2023-11-06 21:33:35 +0900

    4th

--- Study/Form1.cs
+++ Study/Form1.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
 
 namespace Study
 {
@@ -18,17 +16,27 @@ namespace Study
         private Label leffilename;
         private Label gnetfilename;
         private Label libfilename;
+        private Label fileContents;
+        private Label content;
 
         public Form1()
         {
             InitializeComponent();
             this.WindowState = FormWindowState.Maximized;
+
             docfilename = new Label
             {
                 Location = new Point(1000, 100),
                 AutoSize = true
             };
             this.Controls.Add(docfilename);
+
+            content = new Label
+            {
+                Location = new Point(1000, 220), // Adjusted the location for file content display
+                AutoSize = true
+            };
+            this.Controls.Add(content);
 
             ncfilename = new Label
             {
@@ -88,9 +96,33 @@ namespace Study
         {
             string docFilePath = ShowFileOpenDialog();
             docfilename.Text = "doc : " + docFilePath;
+
+            if (!string.IsNullOrEmpty(docFilePath))
+            {
+                string fileContent = ReadExcelFileContents(docFilePath);
+                DisplayFileContents(fileContent, BorderStyle);
+            }
         }
 
-        private void nc_Click(object sender, EventArgs e)
+        private MetroFramework.Forms.MetroFormBorderStyle GetBorderStyle()
+        {
+            return BorderStyle;
+        }
+
+        // Add a method to display file contents
+        private void DisplayFileContents(string content, MetroFramework.Forms.MetroFormBorderStyle borderStyle)
+        {
+            if (fileContents != null)
+            {
+                fileContents.Text = "File Contents:\n" + content;
+                fileContents.AutoSize = false; // Allow multi-line display
+                fileContents.Width = 300; // Adjust the width as needed
+                fileContents.Height = 200; // Adjust the height as needed for display
+                fileContents.Padding = new Padding(5); // Add padding for better appearance
+                fileContents.Font = new Font("Arial", 10); // Setting a specific font
+            }
+        }
+            private void nc_Click(object sender, EventArgs e)
         {
             string ncFilePath = ShowFileOpenDialog();
             ncfilename.Text = "nc : "+ ncFilePath;
@@ -138,6 +170,41 @@ namespace Study
                 return ""; // 취소되었을 때 빈 문자열 반환
             }
         }
+        private string ReadExcelFileContents(string filePath)
+        {
+            string content = "";
+
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook excelWorkbook = excelApp.Workbooks.Open(filePath);
+            Excel._Worksheet excelWorksheet = excelWorkbook.Sheets[1]; // 첫 번째 시트 선택
+
+            Excel.Range excelRange = excelWorksheet.UsedRange;
+
+            int rowCount = excelRange.Rows.Count;
+            int colCount = excelRange.Columns.Count;
+
+            // 이중 for 루프를 사용하여 각 셀의 내용 읽기
+            for (int i = 1; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
+                {
+                    // 각 셀의 내용을 content에 추가
+                    content += excelRange.Cells[i, j].Value2.ToString() + "\t";
+                }
+                content += "\n"; // 다음 행으로 이동
+            }
+
+            // 리소스 정리
+            excelWorkbook.Close();
+            excelApp.Quit();
+
+            ReleaseObject(excelRange);
+            ReleaseObject(excelWorksheet);
+            ReleaseObject(excelWorkbook);
+            ReleaseObject(excelApp);
+
+            return content;
+        }
         private string ReadFileContents(string filePath)
         {
             if (!string.IsNullOrEmpty(filePath) && File.Exists(filePath))
@@ -156,6 +223,23 @@ namespace Study
                 }
             }
             return "Invalid file path or file does not exist";
+        }
+        private void ReleaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                // 로깅 또는 예외 처리
+            }
+            finally
+            {
+                GC.Collect();
+            }
         }
     }
 }
